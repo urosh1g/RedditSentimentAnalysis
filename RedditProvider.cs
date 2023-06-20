5efd15e65e5c2e8db485dffc97dfd9977195f838
Urosh <urosh_h@protonmail.com>
2023-06-20 22:09:20 +0200

    Reddit API client provider

---
+++ RedditProvider.cs
@@ -0,0 +1,27 @@
+namespace RedditProviders;
+
+using Reddit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+public class RedditProvider
+{
+    static RedditClient? client;
+    static IConfiguration config;
+    static object locker = new object();
+    public static RedditClient GetProvider()
+    {
+        lock (locker)
+        {
+            if (client == null)
+            {
+                config = new ConfigurationBuilder().AddJsonFile(Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "appsettings.json"
+                )).AddEnvironmentVariables().Build();
+                client = new RedditClient(appId: config["Reddit:client-id"], refreshToken: config["Reddit:refresh-token"], accessToken: config["Reddit:access-token"], appSecret: config["Reddit:secret"]);
+            }
+        }
+        return client;
+    }
+}
